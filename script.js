502bcda73eb6b3ae8a36c7d728c3e2b974934dd5
itspitso <147466450+itspitso@users.noreply.github.com>
2024-11-15 21:40:46 +0200

    Add heading, grid and grid styles

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.getElementById('container');
-const containerSize = 960;
+const containerSize = 512;
 
 const defaultGrid = 256;
-const squareSize = 60;
+const squareSize = 56;
 let userGrid; 
 
-for (let i = 0; i < defaultGrid; i++) {
+/*for (let i = 0; i < defaultGrid; i++) {
     const div = document.createElement('div');
     div.style.border = '1px solid black';
     div.style.borderRadius = '5px';
@@ -13,4 +13,30 @@ for (let i = 0; i < defaultGrid; i++) {
     div.style.width = `${squareSize}px`;
     div.style.height = `${squareSize}px`;
     container.appendChild(div)
+}*/
+
+const gridSize = 5;
+const sizeOfSquare = (512 / gridSize) - 2;
+
+// Create column containers that will house the squares
+for (let i = 0; i < gridSize; i++) {
+    const columnContainer = document.createElement('div');
+    columnContainer.style.width = containerSize;
+    columnContainer.style.padding = '1px';
+    columnContainer.style.display = 'flex';
+    columnContainer.style.flexDirection = 'row';
+    container.appendChild(columnContainer)
+
+    // Creating the squares in the column container
+    for (let i = 0; i < gridSize; i++) {
+        const div = document.createElement('div');
+        //div.style.border = '0.5px solid black';
+        //div.style.borderRadius = '5px';
+        div.style.backgroundColor = '#111111';
+        div.style.width = `${sizeOfSquare}px`;
+        div.style.height = `${sizeOfSquare}px`;
+        div.style.marginLeft = '0.5px';
+        div.style.marginRight = '0.5px';
+        columnContainer.appendChild(div)
+    }
 }
