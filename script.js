e642ed16c2e1684ba34799b487c413569a478332
itspitso <147466450+itspitso@users.noreply.github.com>
2024-11-15 14:34:57 +0200

    Add functionality that dynamically calculates square size

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const container = document.getElementById('container');
+const containerSize = 960;
 
-let defaultGrid = 9;
+const defaultGrid = 256;
+const squareSize = 60;
+let userGrid; 
 
 for (let i = 0; i < defaultGrid; i++) {
     const div = document.createElement('div');
     div.style.border = '1px solid black';
+    div.style.borderRadius = '5px';
     div.style.backgroundColor = 'blue';
-    div.style.width = '300px';
-    div.style.height = '300px';
+    div.style.width = `${squareSize}px`;
+    div.style.height = `${squareSize}px`;
     container.appendChild(div)
 }
