2924f300ad59b04e63e02c1419cedfd87525de21
itspitso <147466450+itspitso@users.noreply.github.com>
2024-11-16 20:49:37 +0200

    Add function to generate random colors on hover

--- script.js
+++ script.js
@@ -31,7 +31,15 @@ function createGrid(gridSize) {
             columnContainer.appendChild(div);
             div.onmouseover = changeColour;
             function changeColour() {
-                div.style.backgroundColor = 'yellow';
+                div.style.backgroundColor = generateColor();
+            }
+            function generateColor() {
+                const hexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+                let code = "";
+                for(let i=0; i<6; i++){
+                 code += hexArray[Math.floor(Math.random()*16)];
+                }
+                return `#${code}`
             }
             const gridButton = document.getElementById('grid-size');    
             gridButton.onclick = defineGridSize;
