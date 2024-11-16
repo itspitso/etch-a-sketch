976a084cdad290397987aa2f9d6fc0921550e9f8
itspitso <147466450+itspitso@users.noreply.github.com>
2024-11-16 18:20:13 +0200

    Add function to change grid size based on user input

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
 const container = document.getElementById('container');
 const containerSize = 512;
 
-const defaultGrid = 256;
-const squareSize = 56;
-let userGrid; 
-
-/*for (let i = 0; i < defaultGrid; i++) {
-    const div = document.createElement('div');
-    div.style.border = '1px solid black';
-    div.style.borderRadius = '5px';
-    div.style.backgroundColor = 'blue';
-    div.style.width = `${squareSize}px`;
-    div.style.height = `${squareSize}px`;
-    container.appendChild(div)
-}*/
-
-const gridSize = 16;
-const sizeOfSquare = (512 / gridSize) - 2;
+const defaultGrid = 16;
+let gridSize = 16;
+const sizeOfSquare = (containerSize / gridSize) - 2;
+createGrid(defaultGrid);
 
 // Create column containers that will house the squares
-for (let i = 0; i < gridSize; i++) {
-    const columnContainer = document.createElement('div');
-    columnContainer.style.width = containerSize;
-    columnContainer.style.padding = '1px';
-    columnContainer.style.display = 'flex';
-    columnContainer.style.flexDirection = 'row';
-    container.appendChild(columnContainer)
-
-    // Creating the squares in the column container
+function createGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
-        const div = document.createElement('div');
-        div.className = 'squares';
-        console.log(div.className);
-        //div.style.border = '0.5px solid black';
-        //div.style.borderRadius = '5px';
-        div.style.backgroundColor = '#111111';
-        div.style.width = `${sizeOfSquare}px`;
-        div.style.height = `${sizeOfSquare}px`;
-        div.style.marginLeft = '0.5px';
-        div.style.marginRight = '0.5px';
-        columnContainer.appendChild(div);
-        div.onmouseover = changeColour;
-        function changeColour() {
-            div.style.backgroundColor = 'yellow';
+        const columnContainer = document.createElement('div');
+        columnContainer.style.width = containerSize;
+        columnContainer.style.padding = '1px';
+        columnContainer.style.display = 'flex';
+        columnContainer.style.flexDirection = 'row';
+        container.appendChild(columnContainer)
+
+        // Creating the squares in the column container
+        for (let i = 0; i < gridSize; i++) {
+            const div = document.createElement('div');
+            div.className = 'squares';
+            console.log(div.className);
+            //div.style.border = '0.5px solid black';
+            //div.style.borderRadius = '5px';
+            div.style.backgroundColor = '#111111';
+            div.style.width = `${sizeOfSquare}px`;
+            div.style.height = `${sizeOfSquare}px`;
+            div.style.marginLeft = '0.5px';
+            div.style.marginRight = '0.5px';
+            columnContainer.appendChild(div);
+            div.onmouseover = changeColour;
+            function changeColour() {
+                div.style.backgroundColor = 'yellow';
+            }
+            const gridButton = document.getElementById('grid-size');    
+            gridButton.onclick = defineGridSize;
+
+            function defineGridSize() {
+            gridSize = Number(prompt('Enter the number of squares per side for the new grid. The maximum is 100'));
+            //const squares = document.getElementsByClassName('squares');
+            container.replaceChildren();
+            createGrid(gridSize);
+            }
         }
+        
     }
 }
